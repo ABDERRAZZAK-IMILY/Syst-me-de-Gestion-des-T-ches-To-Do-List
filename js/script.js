e1cc34b7a984fd70e47e77ad4e73b7710755369a
abderrazzak imily <imily2024@gmail.com>
2024-10-28 17:26:01 +0100

    Create Home Page: Implement homepage with HTML and Tailwind CSS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 var intas = document.getElementById('input-task');
 
 function add(){
-    for(let i=0; i<localStorage.length; i++) {
+    for(let i=0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
         alert(`${key}: ${localStorage.getItem(key)}`);
       }
    
       setItem(name, value)
+}
+
+
+
+document.getElementById("myBtn2").addEventListener("click", hiden);
+
+function hiden() {
+  document.getElementById("crud-modal").style.display = "hidden";
 }
