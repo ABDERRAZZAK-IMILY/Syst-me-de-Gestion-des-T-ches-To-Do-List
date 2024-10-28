efa9e42ab34af63b0833c5324eb5e3bf9ed52a3e
abderrazzak imily <imily2024@gmail.com>
2024-10-28 16:50:15 +0100

    Initial Commit: Set up project structure with basic HTML, CSS, and JavaScript files.

---
+++ script.js
@@ -0,0 +1,10 @@
+var intas = document.getElementById('input-task');
+
+function add(){
+    for(let i=0; i<localStorage.length; i++) {
+        let key = localStorage.key(i);
+        alert(`${key}: ${localStorage.getItem(key)}`);
+      }
+   
+      setItem(name, value)
+}
